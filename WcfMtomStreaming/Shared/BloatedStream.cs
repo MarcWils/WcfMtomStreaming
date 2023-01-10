aea9c36e3902b8cc347420c870fc79c56d38cbcd
Marc Wils <marc.wils@gmail.com>
2023-01-10 20:50:34 +0100

    Repro to show MTOM streaming in NET6 is not fully supported yet

---
+++ WcfMtomStreaming/Shared/BloatedStream.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Shared
+{
+    public class BloatedStream : Stream
+    {
+        private readonly long length;
+        private long position;
+
+        public BloatedStream(long length)
+        {
+            this.length = length;
+            position = 0;
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => false;
+
+        public override long Length => throw new NotSupportedException();
+
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+
+        public override void Flush()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (position < length)
+            {
+                var numberOfBytesInThisRead = (int)Math.Min(count, length - position);
+                position += numberOfBytesInThisRead;
+                return numberOfBytesInThisRead;
+            }
+
+            return 0;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ WcfMtomStreaming/WcfMtomStreaming.FullFramework/Program.cs
@@ -0,0 +1,77 @@
+﻿using Shared;
+using System;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfMtomStreaming.FullFramework
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Starting...");
+            using (var host = new ServiceHost(typeof(StreamingService)))
+            {
+                host.AddServiceEndpoint(typeof(IStreamingService),
+                    CreateServerBinding(),
+                    "http://localhost:9183");
+                host.Description.Behaviors.OfType<ServiceDebugBehavior>().Single().IncludeExceptionDetailInFaults = true;
+                host.Open();
+
+                var cf = new ChannelFactory<IStreamingService>(CreateClientBinding());
+                cf.Endpoint.Address = host.Description.Endpoints[0].Address;
+                var client = cf.CreateChannel();
+
+                Console.WriteLine("Calling service...");
+
+                await client.UploadAsync(new BloatedStream(4_294_967_296));
+
+                Console.WriteLine("Done, shutting down...");
+                host.Close();
+            }
+        }
+
+        private static Binding CreateServerBinding()
+        {
+            var binding = new CustomBinding();
+            var mtomElement = new MtomMessageEncodingBindingElement(MessageVersion.Soap11, Encoding.Unicode);
+            mtomElement.ReaderQuotas.MaxStringContentLength = 10971520;
+            mtomElement.ReaderQuotas.MaxArrayLength = 10_485_760;
+
+            binding.Elements.Add(mtomElement);
+            binding.Elements.Add(new HttpTransportBindingElement
+            {
+                TransferMode = TransferMode.Streamed,
+                MaxBufferSize = 1024 * 64,
+                MaxBufferPoolSize = 1,
+                MaxReceivedMessageSize = 5_368_709_120
+            });
+            binding.SendTimeout = TimeSpan.FromMinutes(5);
+            binding.ReceiveTimeout = TimeSpan.FromMinutes(5);
+            return binding;
+        }
+
+        private static Binding CreateClientBinding()
+        {
+            var binding = new CustomBinding();
+            var mtomElement = new MtomMessageEncodingBindingElement(MessageVersion.Soap11, Encoding.UTF8);
+            mtomElement.ReaderQuotas.MaxStringContentLength = 10971520;
+            mtomElement.ReaderQuotas.MaxArrayLength = 10_485_760;
+
+            binding.Elements.Add(mtomElement);
+            binding.Elements.Add(new HttpTransportBindingElement
+            {
+                TransferMode = TransferMode.Streamed,
+                MaxBufferSize = 1024 * 64,
+                MaxBufferPoolSize = 1
+            });
+            binding.SendTimeout = TimeSpan.FromMinutes(5);
+            binding.ReceiveTimeout = TimeSpan.FromMinutes(5);
+            return binding;
+        }
+    }
+}
